31fbfd5a48de55da8991e59af56f881469330bb4
alexsousadev <alexandrosousapro@gmail.com>
2025-11-27 11:39:19 -0300

    feat: add http helper for standardized api responses

---
+++ back/src/utils/http-helper.ts
@@ -0,0 +1,54 @@
+import { Response } from "express";
+
+// Accepts string, treeifyError, prettifyError and flattenError
+type ErrorResponse = string | string[] | Record<string, any> | Record<string, string[] | undefined>;
+
+export class HttpHelper {
+
+    static notFound(res: Response, message: string, errors?: ErrorResponse) {
+        return res.status(404).json({
+            success: false,
+            message,
+            errors
+        }); 
+    }
+
+    static success(res: Response, data: any, message: string) {
+        return res.status(200).json({
+            success: true,
+            message,
+            data
+        });
+    }
+
+    static created(res: Response, data: any, message: string) {
+        return res.status(201).json({
+            success: true,
+            message,
+            data
+        });
+    }
+
+    static conflict(res: Response, message: string) {
+        return res.status(409).json({
+            success: false,
+            message,
+        });
+    }
+
+    static badRequest(res: Response, message: string, errors: ErrorResponse) {
+        return res.status(400).json({
+            success: false,
+            message,
+            errors
+        });
+    }
+
+    static serverError(res: Response) {
+            return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+export default HttpHelper;
